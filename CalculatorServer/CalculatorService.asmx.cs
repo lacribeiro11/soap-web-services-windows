f43c9101c505df3edfd46c7f5cd1d088ba6628c8
Luis Ribeiro <lacribeiro11@gmail.com>
2021-03-20 14:23:03 -0700

    Add the calculator service

---
+++ CalculatorServer/CalculatorService.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalculatorServer
+{
+    /// <summary>
+    /// Summary description for CalculatorService
+    /// </summary>
+    [WebService(Namespace = "http://calculator.ac.at/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        [WebMethod]
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        [WebMethod]
+        public double Div(int a, int b)
+        {
+            return b == 0 ? 0 : (double)a / (double)b;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,12 +8,27 @@
 	- System.ServiceModel.Http
 	- System.ServiceModel.Primitives
 
+## Change to the properties
+
+1. Open the project properties
+
+1. In the Startup Project select `Multiple startup projects`
+
+1. Set booth projects action to `Start`
+
 ## Calculator Server
 
 1. Create a new Solution: `ASP.NET Web Application (.NET Framework)`
 
 1. Add Web Service (ASMX)
 
+1. Add the new `WebMethod` to the service:
+	- Add(int a, int b): int
+	- Sub(int a, int b): int
+	- Mul(int a, int b): int
+	- Div(int a, int b): int
+
+1. Set the WebService as start page
 
 ## Calculator Client
 
