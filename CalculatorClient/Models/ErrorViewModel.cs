86c10484eed6d9f1eb5a9691c45aa7f634f4f95b
Luis Ribeiro <lacribeiro11@gmail.com>
2021-03-20 13:43:13 -0700

    Add Client Calculator

---
+++ CalculatorClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CalculatorClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # soap-web-services-windows
+
+## Calculator Server
+
+1. Create a new Solution: `ASP.NET Web Application (.NET Framework)`
+
+## Calculator Client
+
+1. Create a new Solution: `ASP.NET Core Web App (MVC)`
